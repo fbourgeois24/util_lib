6b8a545175ce87fe74d6281f763f060b467baec1
fbourgeoi <francois.bourgeois@aiesh.be>
2023-03-15 11:49:07 +0100

    import des fonctions liste

--- __init__.py
+++ __init__.py
@@ -2,9 +2,11 @@ import os
 
 try:
 	# On importe tout
-	from util_lib.util_lib import timer, ping, yaml_parametres, get_ip, get_network_infos, get_hostname, get_username, get_os, supervisor_status, scale, logger
+	from util_lib.util_lib import timer, ping, yaml_parametres, get_ip, get_network_infos, get_hostname, get_username, get_os, supervisor_status, scale, logger, \
+		present_in_list, get_item_in_list
 except ModuleNotFoundError:
 	# Si module non trouvé, on installe les dépendances
 	os.popen(f"pip install --no-cache-dir -r {os.path.dirname(os.path.realpath(__file__))}/requirements.txt").read()
-	from util_lib.util_lib import timer, ping, yaml_parametres, get_ip, get_network_infos, get_hostname, get_username, get_os, supervisor_status, scale, logger
+	from util_lib.util_lib import timer, ping, yaml_parametres, get_ip, get_network_infos, get_hostname, get_username, get_os, supervisor_status, scale, logger, \
+		present_in_list, get_item_in_list
 
